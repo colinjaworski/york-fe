577aa4f80243b99fe8d748b178b16d2188c46ea9
colin jaworski <colinrjaworski.84@gmail.com>
2023-08-22 14:37:38 -0500

    added MUI, changed buttons in App.js, centered w/ css class

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import RPS from './components/RPS'
 import Header from './components/Header'
 import Placeholder from './components/Placeholder';
 import TicTacToe from './components/TicTacToe';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+
+
+
 
 function App() {
   const [gameOption, setGameOption] = useState('');
@@ -17,16 +24,21 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Header/>
+        <Header />
       </Router>
       <Name />
-      <button onClick={() => gameSelection('RPS')}>Rock Paper Scissors</button>
-      <button onClick={() => gameSelection('TicTacToe')}>Tick Tack Toes</button>
-      <button onClick={() => gameSelection('Placeholder')}>This is a placeholder</button>
+      <Stack spacing={1} direction="row" className='gameSelection'>
+        {/* button variant could also be "text" or "contained" */}
+        <Button variant="outlined" onClick={() => gameSelection('RPS')}>Rock Paper Scissors</Button>
+        <Button variant="outlined" onClick={() => gameSelection('TicTacToe')}>Tick Tack Toes</Button>
+        <Button variant="outlined" onClick={() => gameSelection('Placeholder')}>This is a placeholder</Button>
+      </Stack>
       {gameOption === 'RPS' && <RPS />}
       {gameOption === 'TicTacToe' && <TicTacToe />}
       {gameOption === 'Placeholder' && <Placeholder />}
+
     </div>
+
   );
 }
 
